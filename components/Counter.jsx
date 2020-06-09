b7396339a5873572c7353c53b815f8a5ce8dd7a5
YosephAhmed <yosephahmed@aol.com>
2020-06-08 21:32:44 -0400

    Added a pastValues array but it doesn't work atm

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,9 +1,10 @@
 // Create components/Counter.jsx
 import React, { Component } from 'react'
 
-const Counter = ({ value, onIncrement, onIncrementByValue, onIncrementByFive, onDecrement, onClear }) => (
+const Counter = ({ value, pastValues, onIncrement, onIncrementByValue, onIncrementByFive, onDecrement, onClear }) => (
   <div>
     <h1> Value: {value} </h1>
+    <h1> Past: {console.log(pastValues)} </h1>
     <button onClick={onIncrement}> + </button>
     <button onClick={onDecrement}> - </button>
     <button onClick={onClear}> Clear </button>
--- reducers/index.js
+++ reducers/index.js
@@ -1,20 +1,39 @@
 // reducers/index.js
 import { INCREMENT, INCREMENT_BY_FIVE, INCREMENT_BY_VALUE, DECREMENT, CLEAR, } from '../actions';
 
-const initialState = {value:0};
+const initialState = {
+  value: 0,
+  pastValues: [],
+};
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return {value: state.value + 1};
+      return {
+        value: state.value + 1,
+        pastValues: [...state.pastValues, action.payload]
+      };
     case INCREMENT_BY_FIVE:
-      return {value: state.value + 5};
+      return {
+        value: state.value + 5,
+        pastValues: [...state.pastValues, action.payload]
+      };
     case INCREMENT_BY_VALUE:
-      return {value: state.value + action.payload};
+      return {
+        value: state.value + action.payload,
+        pastValues: [...state.pastValues, action.payload]
+      };
       //{value: state.value + action.value};
     case DECREMENT:
-      return {value: state.value - 1};
+      return {
+        value: state.value - 1,
+        pastValues: [...state.pastValues, action.payload]
+      };
     case CLEAR:
-      return {value: 0};
+      return {
+        value: 0,
+        pastValues: []
+      };
     default:
       return state;
   }
